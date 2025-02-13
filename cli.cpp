2720f9b33d1607f906430ed513e7591a391d5561
Nick Waddoups <nick.waddoups@usu.edu>
2025-02-12 20:51:05 -0700

    Aded cli parsing

--- cli.cpp
+++ cli.cpp
@@ -1,4 +1,5 @@
 #include "random_ints.hpp"
+#include <fmt/core.h>
 #include <iostream>
 #include <string>
 #include <type_traits>
@@ -17,6 +18,14 @@ auto print_range(auto &&range)
 
 auto main(int argc, char **argv) -> int
 {
-    auto ints = random_ints(10);
+    if (argc != 3) {
+        fmt::println("Usage: {} <number of threads> <number of ints>", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    auto num_threads = std::stoul(argv[1]);
+    auto num_ints = std::stoul(argv[2]);
+
+    auto ints = random_ints(num_ints);
     print_range(ints);
 }

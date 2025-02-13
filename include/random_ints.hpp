de4973f7be884fc8bee830d9e1236586fd59a56e
Nick Waddoups <nick.waddoups@usu.edu>
2025-02-12 20:37:05 -0700

    Added random int generator header

---
+++ include/random_ints.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <algorithm>
+#include <cstdint>
+#include <cstdlib>
+#include <limits>
+#include <numeric>
+#include <random>
+#include <vector>
+
+struct Rand
+{
+    using utype = uint32_t;
+    using result_type = utype; 
+
+    inline auto operator()() -> utype { return static_cast<utype>(::rand()); }
+    static constexpr auto max() -> utype
+    {
+        return std::max(std::numeric_limits<utype>::max(), static_cast<utype>(RAND_MAX));
+    }
+    static constexpr auto min() -> utype { return 0; }
+};
+static_assert(std::uniform_random_bit_generator<Rand>);
+
+template<size_t SEED = 100>
+[[nodiscard]]
+auto random_ints(const size_t size) -> std::vector<int>
+{
+    ::srand(SEED);
+    std::vector ints(size, 0);
+    std::iota(ints.begin(), ints.end(), 1);
+    std::shuffle(ints.begin(), ints.end(), Rand{});
+
+    return ints;
+}

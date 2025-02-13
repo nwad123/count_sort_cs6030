dcc7f639773c7f86e5df3abbf913fe640c82dbcd
Nick Waddoups <nick.waddoups@usu.edu>
2025-02-12 20:33:15 -0700

    Implemented random array generator

--- cli.cpp
+++ cli.cpp
@@ -1,6 +1,18 @@
+#include "random_ints.hpp"
 #include <iostream>
+#include <string>
 
-auto main() -> int 
+auto main() -> int
 {
-    std::cout << "Hello, world!";
+    auto print_range = [](auto &&range) {
+        std::string sep = "[";
+        for (const auto &x : range) {
+            std::cout << sep << x;
+            sep = ", ";
+        }
+        std::cout << "]\n";
+    };
+
+    auto ints = random_ints(10);
+    print_range(ints);
 }

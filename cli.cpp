3ab3d6b8ab8ac4867ebced14bbd4fc580fd78db2
Nick Waddoups <nick.waddoups@usu.edu>
2025-02-12 20:40:08 -0700

    Added constraint to print range

--- cli.cpp
+++ cli.cpp
@@ -1,10 +1,13 @@
 #include "random_ints.hpp"
 #include <iostream>
 #include <string>
+#include <type_traits>
 
 auto main() -> int
 {
-    auto print_range = [](auto &&range) {
+    auto print_range = [](auto &&range)
+        requires std::ranges::input_range<std::decay_t<decltype(range)>>
+    {
         std::string sep = "[";
         for (const auto &x : range) {
             std::cout << sep << x;

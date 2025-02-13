d07ca51b8aabc2e250ce724f557f7c3a36cb0364
Nick Waddoups <nick.waddoups@usu.edu>
2025-02-12 21:37:17 -0700

    Implemented equality checker

--- cli.cpp
+++ cli.cpp
@@ -1,8 +1,10 @@
 #include "count_sort.hpp"
 #include "count_sort_reference.hpp"
 #include "random_ints.hpp"
+#include <algorithm>
 #include <fmt/core.h>
 #include <fmt/ranges.h>
+#include <iostream>
 #include <string>
 
 auto main(int argc, char **argv) -> int
@@ -16,15 +18,20 @@ auto main(int argc, char **argv) -> int
     auto num_ints = std::stoul(argv[2]);
 
     auto ints = random_ints(num_ints);
+    auto ints2 = random_ints(num_ints);
 
-    fmt::println("Original range:   {}", ints);
+    fmt::println("Random range:     {}\n", ints);
 
-    // create a copy to test the two implementations
-    auto ints_2 = ints;
-
-    count_sort_reference(ints.data(), static_cast<int>(num_ints));
-    count_sort(ints_2, num_threads);
+    count_sort_reference(ints.data(), static_cast<int>(ints.size()));
+    count_sort(ints2, num_threads);
 
     fmt::println("Reference sorted: {}", ints);
-    fmt::println("Parallel sorted:  {}", ints_2);
+    fmt::println("Parallel sorted:  {}", ints2);
+    fmt::println("\nImplementations {} equal", [&]() {
+        if (std::ranges::equal(ints, ints2)) {
+            return "are";
+        } else {
+            return "are not";
+        }
+    }());
 }

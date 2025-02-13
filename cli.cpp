e633865e3037719c7e98062f14f73abbbba66834
Nick Waddoups <nick.waddoups@usu.edu>
2025-02-12 21:56:39 -0700

    Finished sans documentation

--- cli.cpp
+++ cli.cpp
@@ -4,8 +4,18 @@
 #include <algorithm>
 #include <fmt/core.h>
 #include <fmt/ranges.h>
-#include <iostream>
 #include <string>
+
+const size_t MAX_TO_PRINT = 20;
+
+auto p(auto &&range, const size_t max_elem = MAX_TO_PRINT)
+{
+    if (range.size() > max_elem) {
+        return std::span{ range }.subspan(0, max_elem);
+    } else {
+        return std::span{ range };
+    }
+}
 
 auto main(int argc, char **argv) -> int
 {
@@ -17,16 +27,19 @@ auto main(int argc, char **argv) -> int
     auto num_threads = std::stoul(argv[1]);
     auto num_ints = std::stoul(argv[2]);
 
+    if (num_ints > MAX_TO_PRINT) { fmt::println("Large input size, only 20 elements printed\n"); }
+
     auto ints = random_ints(num_ints);
     auto ints2 = random_ints(num_ints);
 
-    fmt::println("Random range:     {}\n", ints);
+    fmt::println("Random range:     {}\n", p(ints));
 
     count_sort_reference(ints.data(), static_cast<int>(ints.size()));
-    count_sort(ints2, num_threads);
+    fmt::println("Reference sorted: {}", p(ints));
 
-    fmt::println("Reference sorted: {}", ints);
-    fmt::println("Parallel sorted:  {}", ints2);
+    count_sort(ints2, num_threads);
+    fmt::println("Parallel sorted:  {}", p(ints2));
+    
     fmt::println("\nImplementations {} equal", [&]() {
         if (std::ranges::equal(ints, ints2)) {
             return "are";
